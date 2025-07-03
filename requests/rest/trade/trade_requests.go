50ddfda078b852e3e9bf16038dcfee055fd0329f
Alex41 <vk.alex41@gmail.com>
2025-07-03 12:50:24 +0200

    [BROK-542]: upd PlaceOrder type

--- requests/rest/trade/trade_requests.go
+++ requests/rest/trade/trade_requests.go
@@ -12,8 +12,8 @@ type (
 		ClOrdID    string            `json:"clOrdId,omitempty"`
 		Tag        string            `json:"tag,omitempty"`
 		ReduceOnly bool              `json:"reduceOnly,omitempty"`
-		Sz         float64           `json:"sz,string"`
-		Px         float64           `json:"px,omitempty,string"`
+		Sz         string            `json:"sz"`
+		Px         string            `json:"px,omitempty"`
 		TdMode     okex.TradeMode    `json:"tdMode"`
 		Side       okex.OrderSide    `json:"side"`
 		PosSide    okex.PositionSide `json:"posSide,omitempty"`
