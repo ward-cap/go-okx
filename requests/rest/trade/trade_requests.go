b2c5d6443c4f32e91e47748a75a160f8f7a936bc
Alex41 <vk.alex41@gmail.com>
2025-09-24 16:20:53 +0200

    [BROK-542]: fix rq

--- requests/rest/trade/trade_requests.go
+++ requests/rest/trade/trade_requests.go
@@ -58,11 +58,15 @@ type (
 		State    okex.OrderState     `json:"state,omitempty"`
 	}
 	TransactionDetails struct {
-		Uly      string              `json:"uly,omitempty"`
-		InstID   string              `json:"instId,omitempty"`
-		OrdID    string              `json:"ordId,omitempty"`
-		After    float64             `json:"after,omitempty,string"`
-		Before   float64             `json:"before,omitempty,string"`
+		AfterBillID  string `json:"after,omitempty"`
+		BeforeBillID string `json:"before,omitempty"`
+
+		BeginUnixTime int64 `json:"begin,omitempty"`
+		EndUnixTime   int64 `json:"end,omitempty"`
+
+		InstID string `json:"instId,omitempty"`
+		OrdID  string `json:"ordId,omitempty"`
+
 		Limit    float64             `json:"limit,omitempty,string"`
 		InstType okex.InstrumentType `json:"instType,omitempty"`
 	}
